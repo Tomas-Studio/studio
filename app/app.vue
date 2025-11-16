1e3ff30de94d9bf496fa796ed43a8207308870ec
olakunle.olatunji <olatunji.olakunle@cyphercrescent.com>
2025-11-16 08:12:35 +0100

    chore: configure tailwindcss

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,20 @@
+<script lang="ts" setup>
+
+useHead({
+  htmlAttrs: { lang: 'en', class: 'touch-manipulation' },
+  bodyAttrs: { class: 'touch-manipulation antialiased font-sans text-gray-9 dark:text-gray-3' },
+  link: [{ rel: 'icon', href: '/to-favicon.png' }],
+})
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtRouteAnnouncer>
+    <template #default="{ message }">
+      <p>{{ message }} was loaded.</p>
+    </template>
+  </NuxtRouteAnnouncer>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <section>
+    This is tunji website
+  </section>
+</template>
