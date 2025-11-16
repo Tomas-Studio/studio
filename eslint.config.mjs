466749e71e25e54032e9d45940f2b84e32622f79
olakunle.olatunji <olatunji.olakunle@cyphercrescent.com>
2025-11-16 09:05:26 +0100

    chore: added eslint.config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': 'off',
+    'vue/no-dupe-keys': 'warn',
+    '@typescript-eslint/no-dynamic-delete': 'off',
+  },
+  ignores: ['*.css', '*.png', '*.ico', '*.txt', 'src/public/'],
+})
